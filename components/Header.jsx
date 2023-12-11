812dd62ab3b1d358756a924aa1f9ce7f0495e32b
AriasRafael2612 <115297913+AriasRafael2612@users.noreply.github.com>
2023-12-11 13:24:25 -0800

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,46 +1,51 @@
-import React from 'react';
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
-  return (
-    <header className="bg-gradient-to-r from-gray-900 to-gray-800 p-12">
-      
-        <div className="text-center mt-5">
-          <h1 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold mb-4">AUTOMATAS FINITOS</h1>
+    return (
+        <header className="bg-gradient-to-r from-gray-900 to-gray-800 p-12">
+        <div className="container mx-auto">
+        <div className="text-center mt-15">
+                <h1 className="text-white text-3xl font-bold mb-4"> AUTOMATAS FINITOS</h1>
         </div>
 
         <nav className="space-x-4">
-          <ul className="flex flex-col md:flex-row md:items-center md:justify-evenly uppercase">
+            <ul className='flex justify-evenly uppercase'>
+                <li className ="hover:text-teal-400">
+                    <Link className ="cursor-pointer text-blue-400"  href='/'>Inicio</Link>
+                </li>
 
-            <li className='text-blue-500 hover:scale-110 transition duration-300 py-1 hover:text-teal-400'>
-              <Link href='/'>Inicio</Link>
-            </li>
+                <li className ='hover:scale-25 duration-300 py-1 hover:text-teal-400'>
+                    <Link className ="cursor-pointer text-blue-400" href='/ejercicio01'>Ejercicio 01</Link>
+                </li>
 
-            <li className='text-blue-500 hover:scale-110 transition duration-300 py-1 hover:text-teal-400'>
-              <Link href='/ejercicio01'>Ejercicio 01</Link>
-            </li>
+                <li className ='hover:scale-25 duration-300 py-1 hover:text-teal-400'>
+                    <Link className ="cursor-pointer text-blue-400" href='/ejercicio02'>Ejercicio 02</Link> 
+                </li>
 
-            <li className='text-blue-500 hover:scale-110 transition duration-300 py-1 hover:text-teal-400'>
-              <Link href='/ejercicio02'>Ejercicio 02</Link>
-            </li>
+                <li className ='hover:scale-25 duration-300 py-1 hover:text-teal-400'>
+                    <Link className ="cursor-pointer text-blue-400" href='/ejercicio03'>Ejercicio 03</Link> 
+                </li>
 
-            <li className='text-blue-500 hover:scale-110 transition duration-300 py-1 hover:text-teal-400'>
-              <Link href='/ejercicio03'>Ejercicio 03</Link>
-            </li>
+                <li className ='hover:scale-25 duration-300 py-1 hover:text-teal-400'>
+                    <Link className ="cursor-pointer text-blue-400" href='/ejercicio04'>Ejercicio 04</Link> 
+                </li>
+                        
+                <li className ='hover:scale-25 duration-300 py-1 hover:text-teal-400'>
+                    <Link className ="cursor-pointer text-blue-400" href='/ejercicio05'>Ejercicio 05</Link> 
+                </li>
 
-            <li className='text-blue-500 hover:scale-110 transition duration-300 py-1 hover:text-teal-400'>
-              <Link href='/ejercicio04'>Ejercicio 04</Link>
-            </li>
+                <li className ='hover:scale-25 duration-300 py-1 hover:text-teal-400'>
+                    <Link className ="cursor-pointer text-blue-400" href='/ejercicio06'>Ejercicio 06</Link> 
+                </li>
 
-            <li className='text-blue-500 hover:scale-110 transition duration-300 py-1 hover:text-teal-400'>
-              <Link href='/ejercicio05'>Ejercicio 05</Link>
-            </li>
 
-          </ul>
+            </ul>
         </nav>
+    </div>
+    </header>      
      
-    </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
